f46e4f24a6703dfd54696afa86354c269efa267b
bobby <jonas.meier1212@gmail.com>
2018-02-14 23:53:21 +0100

    NodeTree: Implemented AddNode(...), RemoveNode(...), NodeTree() and variables for future size calculation

--- Fancy-SAI-Editor/NodeTree.cs
+++ Fancy-SAI-Editor/NodeTree.cs
@@ -3,13 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace NodeAI
 {
     public class NodeTree
     {
+        public NodeTree()
+        {
+            nodes = new List<Node>();
+            height = 0;
+            width = 0;
+            position = new Point();
+        }
 
+        /// <summary>
+        /// Adds a node to this tree and position it in this tree
+        /// </summary>
+        public void AddNode(Node _node)
+        {
+            nodes.Add(_node);
+
+            //Recalc height and width
+            //TODO!
+        }
+
+        /// <summary>
+        /// Removes the passed node from this tree
+        /// </summary>
+        public void RemoveNode(Node _node)
+        {
+            nodes.Remove(_node);
+        }
 
         private List<Node> nodes; //I don't think this is really needed but eventually it's handy to have
+        private Point position;
+        private int height;
+        private int width;
     }
 }

c90e5f6826ff1e749c7fd02fed99b1c9bd835d8e
Bobby <jonas.meier1212@gmail.com>
2018-02-21 13:23:02 +0100

    SAIImporter: Select params on import

--- Fancy-SAI-Editor/SAIImporter.cs
+++ Fancy-SAI-Editor/SAIImporter.cs
@@ -23,59 +23,101 @@ namespace NodeAI
                 MessageBox.Show("This Npc is not a valid SAI owner!");
                 return;
             }
-            NodeManager.Instance.AddNode(saiOwnerNode);
-            foreach (DataRow row in data.Rows)
+
+            try
             {
-                Node eventNode = null;
-                Node actionNode = null;
-                Node targetNode = null;
-                int event_type = Convert.ToInt32(row["event_type"]);
-                int action_type = Convert.ToInt32(row["action_type"]);
-                int target_type = Convert.ToInt32(row["target_type"]);
-                
-                //Search the nodes with these types
-                Assembly assembly = Assembly.GetExecutingAssembly();
-                foreach (var type in assembly.GetTypes())
+                NodeManager.Instance.AddNode(saiOwnerNode);
+                (saiOwnerNode as Nodes.GeneralNodes.GeneralNode).SetParamValue(entry.ToString());
+                foreach (DataRow row in data.Rows)
                 {
-                    if (type.GetCustomAttribute<NodeAttribute>() is NodeAttribute attribute)
+                    Node eventNode = null;
+                    Node actionNode = null;
+                    Node targetNode = null;
+
+                    int event_type = Convert.ToInt32(row["event_type"]);
+                    int action_type = Convert.ToInt32(row["action_type"]);
+                    int target_type = Convert.ToInt32(row["target_type"]);
+
+                    //Search the nodes with these types
+                    Assembly assembly = Assembly.GetExecutingAssembly();
+                    foreach (var type in assembly.GetTypes())
                     {
-                        switch (Node.GetSuperiorType(attribute.Type))
+                        if (type.GetCustomAttribute<NodeAttribute>() is NodeAttribute attribute)
                         {
-                            case NodeType.EVENT:
-                                if (Node.GetRealId(attribute.Type, NodeType.EVENT) == event_type)
-                                    eventNode = Activator.CreateInstance(type) as Node;
-                                break;
-                            case NodeType.ACTION:
-                                if (Node.GetRealId(attribute.Type, NodeType.ACTION) == action_type)
-                                    actionNode = Activator.CreateInstance(type) as Node;
-                                break;
-                            case NodeType.TARGET:
-                                if (Node.GetRealId(attribute.Type, NodeType.TARGET) == target_type)
-                                    targetNode = Activator.CreateInstance(type) as Node;
-                                break;
+                            switch (Node.GetSuperiorType(attribute.Type))
+                            {
+                                case NodeType.EVENT:
+                                    if (Node.GetRealId(attribute.Type, NodeType.EVENT) == event_type)
+                                        eventNode = Activator.CreateInstance(type) as Node;
+                                    break;
+                                case NodeType.ACTION:
+                                    if (Node.GetRealId(attribute.Type, NodeType.ACTION) == action_type)
+                                        actionNode = Activator.CreateInstance(type) as Node;
+                                    break;
+                                case NodeType.TARGET:
+                                    if (Node.GetRealId(attribute.Type, NodeType.TARGET) == target_type)
+                                        targetNode = Activator.CreateInstance(type) as Node;
+                                    break;
+                            }
                         }
                     }
-                }
-                
-                if(eventNode == null)
-                {
-                    MessageBox.Show($"Event node for event type {event_type} can't be created!");
-                    continue;
-                }
-                else if (actionNode == null)
-                {
-                    MessageBox.Show($"Action node for action type {action_type} can't be created!");
-                    continue;
-                }
-                else if (targetNode == null)
-                {
-                    MessageBox.Show($"Target node for target type {target_type} can't be created!");
-                    continue;
-                }
 
-                NodeManager.Instance.AddNode(eventNode, saiOwnerNode);
-                NodeManager.Instance.AddNode(actionNode, eventNode);
-                NodeManager.Instance.AddNode(targetNode, actionNode);
+                    if (eventNode == null)
+                    {
+                        MessageBox.Show($"Event node for event type {event_type} can't be created!");
+                        continue;
+                    }
+                    else if (actionNode == null)
+                    {
+                        MessageBox.Show($"Action node for action type {action_type} can't be created!");
+                        continue;
+                    }
+                    else if (targetNode == null)
+                    {
+                        MessageBox.Show($"Target node for target type {target_type} can't be created!");
+                        continue;
+                    }
+
+                    NodeManager.Instance.AddNode(eventNode, saiOwnerNode);
+                    NodeManager.Instance.AddNode(actionNode, eventNode);
+                    NodeManager.Instance.AddNode(targetNode, actionNode);
+
+                    for (int i = 0; i < 4; ++i)
+                        eventNode.SetParam((ParamId)i, Convert.ToInt32(row["event_param" + (i + 1).ToString()]).ToString());
+
+                    for (int i = 0; i < 6; ++i)
+                        actionNode.SetParam((ParamId)i, Convert.ToInt32(row["action_param" + (i + 1).ToString()]).ToString());
+
+                    for (int i = 0; i < 7; ++i)
+                    {
+                        if(i < 3)
+                            targetNode.SetParam((ParamId)i, Convert.ToInt32(row["target_param" + (i + 1).ToString()]).ToString());
+                        else
+                        {
+                            switch(i)
+                            {
+                                case 3:
+                                    targetNode.SetParam((ParamId)i, Convert.ToInt32(row["target_x"]).ToString());
+                                    break;
+                                case 4:
+                                    targetNode.SetParam((ParamId)i, Convert.ToInt32(row["target_y"]).ToString());
+                                    break;
+                                case 5:
+                                    targetNode.SetParam((ParamId)i, Convert.ToInt32(row["target_z"]).ToString());
+                                    break;
+                                case 6:
+                                    targetNode.SetParam((ParamId)i, Convert.ToInt32(row["target_o"]).ToString());
+                                    break;
+                            }
+                        }
+                    }
+                    
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Invalid SAI!\nError: " + e.Message + "\nStack:\n" + e.StackTrace);
+                return;
             }
         }
     }

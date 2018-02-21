fb8fb7d142f45e12b73c8d74817a6b55c0613e4a
Bobby <jonas.meier1212@gmail.com>
2018-02-22 00:27:36 +0100

    SqlExporter: Finished ExportNodeTree

--- Fancy-SAI-Editor/SqlExporter.cs
+++ Fancy-SAI-Editor/SqlExporter.cs
@@ -35,10 +35,12 @@ namespace NodeAI
             
             try
             {
-                foreach(Node npcNode in npcNodes)
+                foreach(Npc npcNode in npcNodes)
                 {
-                    update += "UPDATE creature_template SET AIName='SmartAI' WHERE entry=@NPC;" + Environment.NewLine;
-                    update += "DELETE FROM smart_scripts WHERE entryorguid=@NPC AND source_type=0;" + Environment.NewLine;
+                    update += "-- " + npcNode.GetNpcName() + " SAI" + Environment.NewLine;
+                    update += "SET @NPC := " + npcNode.GetParamValue() + ";" + Environment.NewLine;
+                    update += "UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=@NPC;" + Environment.NewLine;
+                    update += "DELETE FROM `smart_scripts` WHERE `entryorguid`=@NPC AND `source_type`=0;" + Environment.NewLine;
                     update += "INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES" + Environment.NewLine;
                     int id = 0;
                     foreach (Nodes.EventNodes.EventNode eventNode in npcNode.GetDirectlyConnectedNodes(NodeType.EVENT, NodeConnectorType.OUTPUT))
@@ -90,6 +92,11 @@ namespace NodeAI
                             }
                         }
                     }
+
+                    update = update.TrimEnd(Environment.NewLine.ToCharArray());
+                    update = update.TrimEnd(',');
+                    update += ";";
+                    update += Environment.NewLine;
                 }
 
                 try

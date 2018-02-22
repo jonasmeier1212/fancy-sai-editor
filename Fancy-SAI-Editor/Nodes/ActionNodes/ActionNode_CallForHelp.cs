d38672a36ec188f91c6a9fc0c39e55b5ba892826
Bobby <jonas.meier1212@gmail.com>
2018-02-22 01:01:54 +0100

    Implemented ActionNode_CallForHelp

---
+++ Fancy-SAI-Editor/Nodes/ActionNodes/ActionNode_CallForHelp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace NodeAI.Nodes.ActionNodes
+{
+    [Node(MenuName = "Call for help", Type = NodeType.ACTION_CALL_FOR_HELP, AllowedTypes = new NodeType[] { NodeType.GENERAL_SPELL, NodeType.EVENT, NodeType.TARGET })]
+    class CallForHelp : ActionNode
+    {
+        public CallForHelp()
+        {
+            Type = NodeType.ACTION_CALL_FOR_HELP;
+
+            //Update text
+            NodeName.Content = "Call for help";
+
+            AddParam(ParamId.PARAM_1, "Radius:");
+            AddParam<YesNo>(ParamId.PARAM_2, "Say call for help text:");
+        }
+
+        public override Node Clone()
+        {
+            return new CallForHelp();
+        }
+    }
+}
